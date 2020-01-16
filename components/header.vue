c1ba20d700252760435c3cd69f862024881a9b67
itcast-hsian <541510140@qq.com>
2020-01-16 14:55:22 +0800

    公共头部的布局

--- components/header.vue
+++ components/header.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-      <!-- 头部组件 -->
-      header
-  </div>
+	<div>
+		<!-- logo -->
+		<div class="logo">
+			<img src="/logo.jpg" alt />
+		</div>
+		<!-- 菜单 -->
+		<div>
+			<nuxt-link to="/">首页</nuxt-link>
+			<nuxt-link to="/post">旅游攻略</nuxt-link>
+			<nuxt-link to="/hotel">酒店</nuxt-link>
+			<nuxt-link to="/air">国内机票</nuxt-link>
+		</div>
+        <!-- 登录注册 -->
+		<div>
+			<nuxt-link to="/user/login">登录、注册</nuxt-link>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped lang="less">
 
 </style>
